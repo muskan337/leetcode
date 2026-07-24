192270f2b1fc3d65ea0a90a6fd607db0069f4829
muskan337 <muskanjain.jain-coend@bvp.edu.in>
2026-07-24 21:04:15 +0530

    Time: 42 ms (61.13%), Space: 46.9 MB (33.25%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-       int n = nums.size();
-       int count = 0;
-       vector<int> prefixSum(n, 0);
+     int n= nums.size();
+     int count = 0;
+     vector<int> PS(n, 0);
 
-       prefixSum[0] = nums[0];
-       for(int i =1; i<n; i++){
-        prefixSum[i] = prefixSum[i-1] + nums[i];
-       }
+     PS[0] = nums[0];
+     for(int i=1; i<n; i++){
+        PS[i] = PS[i-1] + nums[i];
+     }
 
-         unordered_map<int, int> m;
-       for(int j=0; j<n; j++){
-        if(prefixSum[j] == k)  count++;
+    unordered_map<int, int> m;
+    for(int j=0; j<n; j++){
+        if(PS[j] == k)  count++;
 
-        int val = prefixSum[j] - k;
+        int val = PS[j] -  k;
         if(m.find(val) != m.end()){
             count += m[val];
         }
-         if(m.find(prefixSum[j]) == m.end()){
-            m[prefixSum[j]]  = 0;
-         }
-         m[prefixSum[j]]++;
-       }
-       return count++;
+         if(m.find(PS[j]) == m.end()){
+          m[PS[j]] = 0;       
+        }
+        m[PS[j]]++;
+    }
+    return count;
     }
 };
