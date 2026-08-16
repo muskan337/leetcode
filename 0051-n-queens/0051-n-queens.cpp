ef0233ccc76f65fdd1b33543668c0207a85395c8
muskan337 <muskanjain.jain-coend@bvp.edu.in>
2026-08-16 13:51:07 +0530

    Time: 3 ms (61.42%), Space: 10 MB (94.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,57 +1,55 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n, string(n, '.'));
-        vector<vector<string>> ans;
-        nQueens(board, ans, 0, n);
-        return ans;
+    vector<vector<string>> solveNQueens(int n){
+     vector<string> board(n, string(n, '.'));
+     vector<vector<string>> ans;
+     nQueens(board, ans, 0, n);
+     return ans;
     }
 
-    bool isSafe(vector<string> board, int r, int c, int n){
-        //horizontal
-        for(int j=0; j<n; j++){
-   if(board[r][j] == 'Q'){
-    return false;
+   bool isSafe(vector<string> &board, int row, int col, int n){
+    //horizontal
+    for(int j=0; j<n; j++){
+        if(board[row][j] == 'Q'){
+            return false;
+        }
+    }
+
+    // vertical
+    for(int i=0; i<n; i++){
+        if(board[i][col] == 'Q'){
+            return false;
+        }
+    }
+
+    //uppper left diagonal
+    for(int i=row, j=col; i>=0 && j>=0; i--, j--){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+ 
+ // upper right diagonal
+    for(int i= row, j=col; i>=0 && j<n; j++, i-- ){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    return true;
    }
-        }
-
-        //vertical
-        for(int i = 0; i<n; i++){
-            if(board[i][c] == 'Q'){
-                return false;
-            }
-        }
-
-       for(int i=r, j=c; i>=0  && j<n; i--,j++){
-        if(board[i][j] == 'Q'){
-            return false;
-        }
-       }
-       
-       
-       for(int i=r, j=c; i>=0 && j>=0; i--,j--){
-        if(board[i][j] == 'Q'){
-            return false;
-        }
-       }
-      
-       return true;
-
-    }   
 
     void nQueens(vector<string> &board, vector<vector<string>> &ans, int row, int n){
-        if(row == n){
+         if(row == n){
             ans.push_back(board);
             return;
-        }
+         }
 
-        for(int j=0; j<n; j++){
-            if (isSafe(board, row, j, n)){
-
-            board[row][j] = 'Q';
+        for(int col=0; col < n; col++){
+            if(isSafe(board, row, col, n)){
+            board[row][col] = 'Q';
             nQueens(board, ans, row+1, n);
-            board[row][j] = '.';
-            }
+            board[row][col] = '.';
+             }
         }
     }
 };
